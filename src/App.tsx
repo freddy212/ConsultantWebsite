b9bf7d2fbaadc91d088ccf183226759b49e248dd
Frederik Consulting <frederikbjornconsulting@gmail.com>
2022-11-07 10:39:51 +0100

    remove algorand code

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,13 @@ import {
   theme,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import { LotteryView } from "./components/LotteryView";
-import { OngoingView } from "./components/OngoingView";
-import { WinnerView } from "./components/WinnerView";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box fontSize="xl">
       <Grid minH="100vh" p={3}>
         <VStack spacing={4}>
-          <LotteryView />
-          <WinnerView />
+          <p>Hello Website</p>
         </VStack>
       </Grid>
     </Box>
